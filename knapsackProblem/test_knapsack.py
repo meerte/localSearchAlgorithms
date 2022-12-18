48ee228a990c8f7cae00f62792eaf659e383a16a
Merthan Erler <mseerler@gmail.com>
2022-12-18 14:28:57 +0300

    Comment lines have been removed

--- knapsackProblem/test_knapsack.py
+++ knapsackProblem/test_knapsack.py
@@ -3,7 +3,7 @@ from simpleai.search import *
 
 
 def main():
-    '''    number_of_items = input('Enter total amount of items: ')
+    number_of_items = input('Enter total amount of items: ')
     number_of_items = int(number_of_items)
     knapsack_capacity = input('Enter knapsack capacity: ')
     knapsack_capacity = int(knapsack_capacity)
@@ -17,13 +17,10 @@ def main():
     for _ in range(number_of_items):
         weight = input(f'Enter value of item{_}: ')
         value = int(weight)
-        value_of_items.append(value)  '''
+        value_of_items.append(value)
 
-    number_of_items = 15
-    knapsack_capacity = 50
-    weight_of_items = [24, 10, 10, 7, 2, 8, 6, 5, 9, 12, 20, 18, 13, 5, 4]
-    value_of_items = [50, 10, 25, 30, 20, 25, 40, 15, 12, 22, 35, 45, 55, 100, 60]
-
+    #  result = hill_climbing_random_restarts(problem, 10, 10, viewer=knapsack_problem.my_viewer)
+    #  result = hill_climbing(problem=problem, iterations_limit=0, viewer=knapsack_problem.my_viewer)
     problem = knapsack_problem.KnapsackProblem(number_of_items=number_of_items,
                                                knapsack_capacity=knapsack_capacity,
                                                weights_of_items=weight_of_items,
